ed48902ed85cce8279d207810a2233b6695ede9f
Unknown426 <yumengyang426@163.com>
2022-04-05 16:15:20 +0800

    lfs管理大文件，修复安装脚本bug和依赖包缺少的内容
    1、重新打包依赖的软件包，之前的缺少依赖包
    2、修复，调整install脚本，优化安装脚本
    3、使用git lfs管理大文件

--- openssh8.4_offline_install/openssh-install.sh
+++ openssh8.4_offline_install/openssh-install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # 离线升级openssh8.4
 
-# 检查用户是否为root
+echo "###########################   检查用户、安装包、准备安装环境   #############################"
 if [ $(id -u) != "0" ]; then
     echo "请使用root执行脚本!! "
     exit 1
 fi
 
+# 获取工作目录
+myPath=`pwd`
 
 #检查安装包：
 openssh="openssh-8.4p1"
@@ -26,17 +28,16 @@ else
   echo 'openssl 已下载'
 fi
 
-dir=$(cd $(dirname "$0");pwd)
-
 #检查，安装依赖包
 if [ ! -f soft.tar.gz ]
 then
   echo ' 相关依赖包不存在，请检查'
 else
   echo '依赖包已下载'
-  tar  -zxvf  soft.tar.gz;cd soft ;rpm  -ivh  ./*rpm
+  tar  -zxvf  soft.tar.gz;cd soft ;yum localinstall   -y   ./*rpm
 fi
 
+cd $myPath
 
 systemctl enable xinetd.service
 systemctl enable telnet.socket
@@ -48,14 +49,12 @@ echo -e 'pts/0\npts/1\npts/2\npts/3'  >>/etc/securetty
 systemctl restart xinetd.service
 echo "telnet 启动成功"
 sleep 3
-echo "########################################################"
 
+echo "###########################开始升级OPENSSL#############################"
 tar xfz ${openssl}.tar.gz
 echo "备份OpenSSL..."
 mv /usr/bin/openssl /usr/bin/openssl_bak
 mv /usr/include/openssl /usr/include/openssl_bak
-
-
 
 echo "开始安装OpenSSL..."
 sleep 3
@@ -63,7 +62,11 @@ cd ${openssl}
 ./config shared --prefix=/usr/local/openssl  && make && make install
 # 注意如果之前编译过openssl 可能无法覆盖，需要更换目录
 
-[ $? -eq 0 ] && echo "openssl安装成功..."
+if [ $? -eq 0 ] ;then
+	echo "openssl安装成功..."
+else
+	echo -e "openssl 安装失败，请检查！" && exit 1
+fi
 
 ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
 ln -s /usr/local/openssl/include/openssl /usr/include/openssl
@@ -73,26 +76,31 @@ echo "/usr/local/openssl/lib" >> /etc/ld.so.conf
 echo "查看确认版本。。。"
 openssl version
 echo "OpenSSL 升级完成..."
-echo "开始升级OPENSSH。。。。。"
 sleep 5
-cd $dir
+
+echo "###########################    开始升级OPENSSH     #############################"
+cd $myPath
 /usr/bin/tar -zxvf ${openssh}.tar.gz
 cd ${openssh}
 chown -R root.root ${openssh}
 ./configure --prefix=/usr/ --sysconfdir=/etc/ssh  --with-openssl-includes=/usr/local/openssl/include \
  --with-ssl-dir=/usr/local/openssl/   --with-zlib   --with-md5-passwords   --with-pam  && make && make install
 
-[ $? -eq 0 ] && echo "openssh 升级成功..."
+if [ $? -eq 0 ] ;then
+	echo "openssh安装成功..."
+else
+	echo -e "openssh 安装失败，请检查！" && exit 1
+fi
+
 cp -a contrib/redhat/sshd.init /etc/init.d/sshd
 cp -a contrib/redhat/sshd.pam /etc/pam.d/sshd.pam
 chmod +x /etc/init.d/sshd
 systemctl enable sshd
 
-#[ $? -eq 0] && echo "sshd服务添加为启动项 ..."
-
+# sshd服务添加为启动项 ...
 mv  /usr/lib/systemd/system/sshd.service  /tmp/
 systemctl restart sshd.service
-netstat -lntp
+ss  -lntp
 echo "查看SSH版本信息。。。"
 ssh -V
 sleep 3
