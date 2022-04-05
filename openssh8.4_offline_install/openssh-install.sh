5f951c4f05058f661a9f51a0a7db1f25f6305de6
Unknown426 <yumengyang426@163.com>
2022-04-05 11:44:16 +0800

    新增openssh8.4 离线自动安装脚本
    新增openssh8.4 离线自动安装脚本

---
+++ openssh8.4_offline_install/openssh-install.sh
@@ -0,0 +1,115 @@
+#!/bin/bash
+# 离线升级openssh8.4
+
+# 检查用户是否为root
+if [ $(id -u) != "0" ]; then
+    echo "请使用root执行脚本!! "
+    exit 1
+fi
+
+
+#检查安装包：
+openssh="openssh-8.4p1"
+openssl="openssl-1.1.1k"
+
+if [ ! -f ${openssh}.tar.gz ]
+then
+  echo ' openssh安装包不存在，请检查'
+else
+  echo ' openssh 已下载'
+fi
+
+if [ ! -f ${openssl}.tar.gz ]
+then
+  echo ' openssl 安装包不存在，请检查'
+else
+  echo 'openssl 已下载'
+fi
+
+dir=$(cd $(dirname "$0");pwd)
+
+#检查，安装依赖包
+if [ ! -f soft.tar.gz ]
+then
+  echo ' 相关依赖包不存在，请检查'
+else
+  echo '依赖包已下载'
+  tar  -zxvf  soft.tar.gz;cd soft ;rpm  -ivh  ./*rpm
+fi
+
+
+systemctl enable xinetd.service
+systemctl enable telnet.socket
+systemctl start telnet.socket
+systemctl start xinetd.service
+
+echo -e 'pts/0\npts/1\npts/2\npts/3'  >>/etc/securetty
+
+systemctl restart xinetd.service
+echo "telnet 启动成功"
+sleep 3
+echo "########################################################"
+
+tar xfz ${openssl}.tar.gz
+echo "备份OpenSSL..."
+mv /usr/bin/openssl /usr/bin/openssl_bak
+mv /usr/include/openssl /usr/include/openssl_bak
+
+
+
+echo "开始安装OpenSSL..."
+sleep 3
+cd ${openssl}
+./config shared --prefix=/usr/local/openssl  && make && make install
+# 注意如果之前编译过openssl 可能无法覆盖，需要更换目录
+
+[ $? -eq 0 ] && echo "openssl安装成功..."
+
+ln -s /usr/local/openssl/bin/openssl /usr/bin/openssl
+ln -s /usr/local/openssl/include/openssl /usr/include/openssl
+echo "加载动态库..."
+echo "/usr/local/openssl/lib" >> /etc/ld.so.conf
+/sbin/ldconfig
+echo "查看确认版本。。。"
+openssl version
+echo "OpenSSL 升级完成..."
+echo "开始升级OPENSSH。。。。。"
+sleep 5
+cd $dir
+/usr/bin/tar -zxvf ${openssh}.tar.gz
+cd ${openssh}
+chown -R root.root ${openssh}
+./configure --prefix=/usr/ --sysconfdir=/etc/ssh  --with-openssl-includes=/usr/local/openssl/include \
+ --with-ssl-dir=/usr/local/openssl/   --with-zlib   --with-md5-passwords   --with-pam  && make && make install
+
+[ $? -eq 0 ] && echo "openssh 升级成功..."
+cp -a contrib/redhat/sshd.init /etc/init.d/sshd
+cp -a contrib/redhat/sshd.pam /etc/pam.d/sshd.pam
+chmod +x /etc/init.d/sshd
+systemctl enable sshd
+
+#[ $? -eq 0] && echo "sshd服务添加为启动项 ..."
+
+mv  /usr/lib/systemd/system/sshd.service  /tmp/
+systemctl restart sshd.service
+netstat -lntp
+echo "查看SSH版本信息。。。"
+ssh -V
+sleep 3
+echo "telnet服务关闭..."
+systemctl disable xinetd.service
+systemctl stop xinetd.service
+systemctl disable telnet.socket
+systemctl stop telnet.socket
+# echo "所有服务升级完成，进行堡垒机连接测试..."
+sleep 3
+# centOS7.4升级openssh8.4
+echo "修改ssh配置..."
+rm -f /etc/ssh/*host*
+
+sed -i 's/#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
+sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+cat /etc/ssh/sshd_config|grep -v "#"  |grep -v "^$" |grep -E "PasswordAuthenticatio|PubkeyAuthentication|PermitRootLogin"
+# echo "PermitRootLogin no"  >> /etc/ssh/sshd_config
+service sshd restart
---
+++ openssh8.4_offline_install/readme.txt
@@ -0,0 +1,2 @@
+注意：
+1、该脚本适用于openssh初始版本为 7.4，如果不是这个版本，请先执行 yum update openssh -y 升级openssh 到7.4
