a053f60da7e6d1a1262bd97e4d73980783c289f0
Unknown426 <yumengyang426@163.com>
2022-06-21 11:49:02 +0800

    redis 安装脚本更新
    1、更新redis 检查脚本，修复无法查到进程的bug
    2、增加哨兵相关说明

--- redis6.2.1_offline_install/check_redis.sh
+++ redis6.2.1_offline_install/check_redis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # check redis
 
-num1=`ps -efww |grep "/usr/local/redis/bin/redis-server /usr/local/redis/etc/redis.conf" |grep -v grep|wc -l`
+num1=`ps -efww |grep "/usr/local/redis/bin/redis-server " |grep -v grep|wc -l`
 
 
 if [ $num1 -lt 1 ];then
